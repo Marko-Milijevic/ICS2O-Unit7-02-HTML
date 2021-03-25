3215df9119067c35821b63f40414eb25d22a40e4
MarkoMilijevic <Marko.Milijevic@ocsbstudent.ca>
2021-03-25 02:19:59 +0000

    Fixed errors

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 /* Connect the button to "comparison" function */
-document.getElementById('button').addEventListener('click',comparison)
-let userInput= 0 
+document.getElementById('button').addEventListener('click', comparison)
+let userInput = 0
 
-function comparison() {
+function comparison () {
   /* Get user input and convert it to an integer */
   userInput = document.getElementById('input').value
   userInput = parseInt(userInput)
 
   /* Check if userInput is less than 0 */
-  if (userInput < 0) {
-    document.getElementById('answer').innerHTML = "Negative"
-  } 
+  if (userInput < 0) 
+  {
+    document.getElementById('answer').innerHTML = 'Negative'
+  }
   /* Check if userInput is greater than or equal to 0 */ 
-  else if (userInput >= 0) {
-    document.getElementById('answer').innerHTML = "Positive"
+  else if (userInput >= 0) 
+  {
+    document.getElementById('answer').innerHTML = 'Positive'
   } 
-  /* Display an error message */
-  else {
-    document.getElementById('answer').innerHTML = "Not a number"
+  /* Else, display an error message */
+  else 
+  {
+    document.getElementById('answer').innerHTML = 'Not an integer'
   }
 }
